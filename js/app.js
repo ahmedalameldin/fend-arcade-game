719f5cf98050b3c70023cfdcfad2801b6aad722e
ahmed-alameldin <ahmed.alameldin@live.com>
2018-08-31 07:32:37 +0100

    Create Hero class
    adding positions, boundary, moving step and jump.
    
    Start creating Enemy functionality

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
+    // X pos
+    // Y pos
+    this.y = 0;
+    this.x = 0;
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +19,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // if enemy is not passed boundary
+        // move forward
+        // increment x by speed * dt
+    // else
+        // reset to start point
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,10 +36,88 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+// Player Class "Hero"
+class Hero {
+    constructor() {
+        // Top left corner of the board
+        this.x = 0;
+        this.y = 0;
+
+        // Spaces between blocks, this numbers comes from engine.js
+        this.jump = 83;
+        this.step = 101;
+
+        // Start place on the bottmo middle row
+        this.startX = 202;
+        this.startY = 405;
+
+        // update the init location for our player "Hero"
+        this.x = this.startX;
+        this.y = this.startY;
+
+        // This is the image of the player
+        this.sprite = "images/char-boy.png";
+
+    }
+
+    // Constructor
+
+        // Properties :
+            // x pos
+            // y pos
+            // sprite img
+
+        // Methods :
+
+            // update()
+                // check collasion here
+                // did player x and y collide with enemy?
+                // did the player touch the top row?
+
+            // render()
+                // Draw player sprite on current x & y position?
+                render() {
+                    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+                }
+
+            // handleInput()
+                // handle keyboard input to move the player x or y
+                // @param {string} input - Direction to travel
+
+                handleInput(input) {
+                    switch(input) {
+                        case 'left' : if (this.x > 0) { this.x -= this.step }
+                        break;
+                        case 'up' : if (this.y > 0) { this.y -= this.jump }
+                        break;
+                        case 'right' : if (this.x < this.step * 4) { this.x += this.step }
+                        break;
+                        case 'down' : if (this.y < this.jump * 4) { this.y += this.jump }
+                        break;
+
+                    } console.log(`x: ${this.x} | y: ${this.y}`);
+                }
+
+            // Reset
+                // Reset the position x & y to the player to old location
+
+}
+
+const player = new Hero();
+const bugTest = new Enemy();
+const allEnemies = [];
+allEnemies.push(bugTest);
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+    // New player object "Hero"
+
+    // init allEnemies array
+    // for each enemy create & push Enemy object into above array
+
 
 
 
@@ -44,3 +133,4 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
